e446e6a2015396fd3f698864df949d06aae7ba1f
Derick Moncado <derickmoncado@gmail.com>
2021-04-17 16:25:23 -0700

    Started functionality, added service and methods with dummy data

---
+++ src/app/shared/data-service.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataServiceService {
+  // Properties
+  public todos: Todo[] = [
+    { text: 'test data 1', completed: true },
+    { text: 'test data 2', completed: false },
+    { text: 'test data 3', completed: true },
+    { text: 'test data 4', completed: false },
+    { text: 'test data 5', completed: false },
+  ];
+
+  constructor() {}
+
+  // Get all todos
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  // Add todo
+  addTodo(todo: Todo): void {
+    this.todos.push(todo);
+  }
+
+  // Update todo
+  updateTodo(index: number, updatedTodo: Todo): void {
+    this.todos[index] = updatedTodo;
+  }
+
+  // Delete todo
+  deleteTodo(index: number): void {
+    this.todos.splice(index, 1);
+  }
+}
