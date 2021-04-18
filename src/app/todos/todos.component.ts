bc8cdee6ac70afaa9dcc7a470cbfc1a34145cd6c
Derick Moncado <derickmoncado@gmail.com>
2021-04-17 18:41:47 -0700

    Set up form submit

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../shared/todo.model';
 import { DataService } from '../shared/data-service.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todos',
@@ -12,6 +13,12 @@ export class TodosComponent implements OnInit {
   todos: Todo[] = [];
 
   constructor(private dataService: DataService) {}
+
+  // Form submit
+  onFormSubmit(form: NgForm): void {
+    console.log('form submitted');
+    console.log(form);
+  }
 
   ngOnInit(): void {
     this.todos = this.dataService.getAllTodos();
